64414bdb69b1f444fef9d0b650ae35a930ed1618
NeptunesOrcaII <orcabot002@gmail.com>
2024-07-24 16:09:20 -0600

    Added Greedy Algorithm problem description and created framework for the Scheduler class for the solution

---
+++ greedyalgthms.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "scheduler.hpp"
+
+/*
+    == QUESTION 1: CHAOS UNIVERSITY ==
+    - courses don't follow an hourly schedule
+    - goal: take as many courses as possible
+    - will need to fit all courses between some arbitrary start and end time
+    - will need to leave at least 10 time units between courses
+    - courses are represented as with two integers between 0 - 99 999, representing the start time and end time
+    - scheduler class should be initialized with a classlist
+    - scheduler class should have a function to calculate the max classes that can be taken
+*/
+
---
+++ scheduler.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+#include "scheduler.hpp"
+
+Scheduler::Scheduler() {
+    
+}
