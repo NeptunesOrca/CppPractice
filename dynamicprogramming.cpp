cf43fbb784ed22f487503ba586607388450380b4
NeptunesOrcaII <orcabot002@gmail.com>
2024-07-27 19:24:38 -0600

    Added problem statement for the dynamic programming question

---
+++ dynamicprogramming.cpp
@@ -0,0 +1,11 @@
+/*
+    == QUESTION 3: COIN GAME ==
+    - Inputs:
+        - array of n positive integers between 0 - 99 999, representing coin values
+        - start int representing which of the coins in the array is the first one available
+        - end int representing which of the coins in the array is the last one avaialable
+    - Outputs:
+        - max: max total sum that can be won by the current player, even if opponent plays optimally
+        - margin: margin by which current player will win if both players win optimally
+        - take_right: boolean of which coin the current player should take to obtain maximum
+*/
